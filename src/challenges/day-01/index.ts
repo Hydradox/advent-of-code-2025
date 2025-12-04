2b5c0c39e57042209be0c86766ffd240d5adf012
Jaime Cantos Martinez <jcantosm@mercadona.es>
2025-12-04 12:55:51 +0100

    feat: partial initial solution

--- src/challenges/day-01/index.ts
+++ src/challenges/day-01/index.ts
@@ -1,4 +1,64 @@
 import logger from '@/lib/logger';
+import { readFileSync } from 'node:fs';
+import path from 'node:path';
 
-logger.info('Hello, World in Day 01!');
-throw new Error('Not implemented yet!');
+const turnLeft = (current: number, movements: number, clicks: { value: number }): number => {
+  let position = current - movements;
+
+  if (position < 0) {
+    clicks.value += 1;
+    position += 100;
+  }
+
+  return position < 0
+    ? turnLeft(position, 0, clicks)
+    : position;
+};
+
+const turnRight = (current: number, movements: number, clicks: { value: number }): number => {
+
+  let position = current + movements;
+
+  if (position >= 100) {
+    clicks.value += 1;
+    position -= 100;
+  }
+
+  return position >= 100
+    ? turnRight(position, 0, clicks)
+    : position;
+};
+
+// Read files
+const inputs: string[] = readFileSync(path.join(process.cwd(), 'src', 'challenges', 'day-01', 'inputs.txt'), { encoding: 'utf8' })
+  .split('\n')
+  .filter(line => line.trim().length > 0);
+
+// Variables
+let zeroCount = 0;
+const clicks = { value: 0 };
+let dial = 50;
+
+// Parse inputs
+for (const input of inputs) {
+  const operation = input.charAt(0);
+  const movements = Number.parseInt(input.slice(1), 10);
+
+  // Left turn
+  if (operation === 'L') {
+    dial = turnLeft(dial, movements, clicks);
+  }
+
+  // Right turn
+  if (operation === 'R') {
+    dial = turnRight(dial, movements, clicks);
+  }
+
+  if (dial === 0) {
+    zeroCount += 1;
+    clicks.value += 1;
+  }
+}
+
+logger.info('Zero count:', zeroCount);
+logger.info('Clicks count:', clicks.value);
